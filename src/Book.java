3ab974be33164e4c06250d7c356ee45bad5a5cb5
seeleytc <seeleytc@s.dcsdk12.org>
2021-03-09 09:38:13 -0700

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    String name;
+    double price;
+    String isbn;
+    public Book(String newName,
+                double newPrice,
+                String newIsbn) {
+        name = newName;
+        price = newPrice;
+        isbn = newIsbn;
+    }
+
+}
---
+++ src/Bookstore.java
@@ -0,0 +1,18 @@
+public class Bookstore {
+    Book[]
+
+    public static void main(String[] args) {
+
+    }
+    public void populateAvailableBooks() {
+
+
+    }
+    public void listAvailableBooks() {
+
+    }
+    public void determineBookSelection() {
+
+    }
+
+}
